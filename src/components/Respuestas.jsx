63c374a147ef9bd4816125f7c5677b0e8923de19
mterrera29 <matiasterrera29@gmail.com>
2025-03-08 00:10:58 -0300

    shuffled answer

--- src/components/Respuestas.jsx
+++ src/components/Respuestas.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { RespuestasCaja } from './RespuestasCaja'
+import React from 'react';
+import { RespuestasCaja } from './RespuestasCaja';
 
-export const Respuestas = ({consignasData}) => {
-  const answers = consignasData.answer
+export const Respuestas = ({ consignasData }) => {
+  function shuffleArray(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+  const answers = shuffleArray(consignasData.answer);
+
   return (
-  <div className='respuestas'>
-    {answers.map((a, indice)=> (
-      <RespuestasCaja a={a} key={indice} indice={indice} consignasData={consignasData} />
-    ))}
-  </div>
-  )
-}
+    <div className='respuestas'>
+      {answers.map((a, indice) => (
+        <RespuestasCaja
+          a={a}
+          key={indice}
+          indice={indice}
+          consignasData={consignasData}
+        />
+      ))}
+    </div>
+  );
+};
